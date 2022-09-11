1bbe1d13014e1951abd0265584031630b8af01bf
Kyle Moore <20785882+YourFavoriteKyle@users.noreply.github.com>
2022-09-11 18:29:06 -0400

    Vercel SMTP Bug Resolved
    Added .vercel to .gitignore

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -17,20 +17,18 @@ export const actions = {
 	default: async ({ request }: any) => {
 		const body = formBody(await request.formData());
 
-		const message = await transporter.sendMail(
-			{
-				from: EMAIL,
-				to: EMAIL,
-				subject: `${body.subject}`,
-				text: `${body.message}\n\nSent from ${body.name} with return address ${body.email}\n\nSent from portfolio contact form.`
-			},
-			function (err, info) {
-				if (err) {
-					return { success: false };
-				} else {
-					return { success: true };
-				}
-			}
-		);
+		const message = await transporter.sendMail({
+			from: EMAIL,
+			to: EMAIL,
+			subject: `${body.subject}`,
+			text: `${body.message}\n\nSent from ${body.name} with return address ${body.email}\n\nSent from portfolio contact form.`
+		});
+		if (message.accepted) {
+			return { success: true, message: message.response };
+		}
+		if (message.rejected) {
+			return { success: false, message: message.response };
+		}
+		return { success: false, message: 'Unexpected error or response.' };
 	}
 };
